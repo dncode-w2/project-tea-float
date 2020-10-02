c5b627529b4d918f9558d245c5f21a63d1ed1c98
Wira Wardhana <wiwa.linuxgeek@gmail.com>
2020-10-02 20:41:42 +0700

    tea

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("navbar");
-const navClose = document.getElementById("nav-close");
+// setup date
+const date = (document.getElementById(
+  'date'
+).innerHTML = new Date().getFullYear());
 
-navBtn.addEventListener("click", () => {
-  navbar.classList.add("showNav");
+const navBtn = document.getElementById('navbar-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
+
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav');
 });
 
-navClose.addEventListener("click", () => {
-  navbar.classList.remove("showNav");
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav');
 });
